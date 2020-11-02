0556d05a9be9a287ad5ba4938837aad7309eab75
bengroves <bengroves15@gmail.com>
2020-11-02 11:45:54 -0500

    commit tree 8eb786d4a95e0375f52be50cac0afb77966f1ba3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+     return self.end_with?"."
   end
 
   def question?
-
+    return self.end_with?"?"
   end
 
   def exclamation?
-
+    return self.end_with?"!"
   end
 
   def count_sentences
-
+    bin = []
+    check1 = self.end_with?"."
+    check2 = self.end_with?"?"
+    check3 = self.end_with?"!"
+    if self == ""
+      return 0
+    elsif check3 == false && check1 == false
+      return self.split(".").count()
+    else 
+      return self.gsub("?",".").split("!").join("").split(".").count+1
+    end 
   end
 end
